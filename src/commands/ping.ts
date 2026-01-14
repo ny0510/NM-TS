a1343dacd339f65bde3f0ccad97f538758f403c1
ny64 <me@ny64.kr>
2026-01-15 04:19:17 +0900

    fix: 지연시간 계산 로직 수정

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,8 @@
 import {ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder} from 'discord.js';
 import {DateTime} from 'luxon';
 
-import type {Command} from '@/client/types';
 import type {NMClient} from '@/client/Client';
+import type {Command} from '@/client/types';
 
 export default {
   data: new SlashCommandBuilder().setName('ping').setDescription('봇의 지연시간을 확인해요.'),
@@ -13,11 +13,16 @@ export default {
     const start = DateTime.now();
     await interaction.deferReply();
 
-    const botLatency = DateTime.now().diff(start).toMillis();
     const apiLatency = client.ws.ping;
 
     await interaction.editReply({
-      embeds: [new EmbedBuilder().setTitle('🏓 당신은 퐁입니다').setDescription(`⏱️ 봇 지연시간: **${botLatency}ms**\n⌛ API 지연시간: **${apiLatency}ms**`).setColor(client.config.EMBED_COLOR_NORMAL).setTimestamp()],
+      embeds: [
+        new EmbedBuilder()
+          .setTitle('🏓 당신은 퐁입니다')
+          .setDescription(`⏱️ 봇 지연시간: **${DateTime.now().diff(start).toMillis()}ms**\n⌛ API 지연시간: **${apiLatency}ms**`)
+          .setColor(client.config.EMBED_COLOR_NORMAL)
+          .setTimestamp(),
+      ],
     });
   },
 } as Command;
