d25b9f6c2c5c77e121fa5e207bc8533d9c9b2232
ny64 <me@ny64.kr>
2026-04-19 14:43:11 +0900

    chore: env.d.ts 업데이트

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     DISCORD_TOKEN: string;
     DISCORD_CLIENT_ID: string;
     DISCORD_GUILD_ID: string;
+    DISCORD_LOG_WEBHOOK_URL?: string;
 
     LAVALINK_IDENTIFIER: string;
     LAVALINK_HOST: string;
@@ -14,10 +15,20 @@ declare namespace NodeJS {
     EMBED_COLOR_ERROR: string;
 
     LOG_PREFIX: string;
-    DEFAULT_VOLUME: string;
+    DEFAULT_VOLUME?: string;
+    NODE_ENV?: 'development' | 'production';
+
+    PLAYER_STATE_PATH?: string;
 
     KOREANBOTS_TOKEN?: string;
     KOREANBOTS_CLIENT_ID?: string;
     KOREANBOTS_UPDATE_INTERVAL?: string;
+
+    PROGRESS_CIRCLE_START?: string;
+    PROGRESS_CIRCLE_MIDDLE?: string;
+    PROGRESS_FILLED_START?: string;
+    PROGRESS_FILLED_MIDDLE?: string;
+    PROGRESS_UNFILLED_MIDDLE?: string;
+    PROGRESS_UNFILLED_END?: string;
   }
 }
